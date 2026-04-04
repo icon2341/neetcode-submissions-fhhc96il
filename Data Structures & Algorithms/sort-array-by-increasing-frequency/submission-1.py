1b88db7025e148c5de71330840a70047d18c5e46
Ashar Rahman <asharrahman2002@gmail.com>
2026-04-04 14:11:00 -0400

    Add: sort-array-by-increasing-frequency - submission-1

---
+++ Data Structures & Algorithms/sort-array-by-increasing-frequency/submission-1.py
@@ -0,0 +1,24 @@
+from collections import Counter
+import heapq
+
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        count = Counter(nums)
+
+        res = []
+
+        temp = []
+
+        for num in count:
+            heapq.heappush(temp,(count[num], -num))
+
+        
+        while temp:
+            tup = heapq.heappop(temp)
+            for i in range(tup[0]):
+                res.append(-tup[1])
+
+        return res
+
+        # bucket sort might be more efficient here.
+        
